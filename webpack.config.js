0324ebe3bf96dab667cc9e64bf2b31025f6e842f
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-03-01 17:36:47 +0100

    added config files

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// Export Files
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        entry: ['babel-polyfill', './src/app.js'],
+        output: {
+            path: path.join(__dirname, 'assets'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: { sourceMap: true }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: { sourceMap: true }
+                        },
+                    ],
+                }),
+            }]
+        },
+        plugins: [
+            CSSExtract,
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map'
+    };
+};
