5bc87c784fd4797031f153a7b09efaa5cd50a8a4
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-05-09 10:14:33 +0200

    added postcss + autoprefix and file loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 // Export Files
 module.exports = (env) => {
     const isProduction = env === 'production';
-    const CSSExtract = new ExtractTextPlugin('styles.css');
+    const CSSExtract = new ExtractTextPlugin('styles.css', { allChunks: true });
 
     return {
         entry: ['babel-polyfill', './src/app.js'],
@@ -27,11 +28,27 @@ module.exports = (env) => {
                             options: { sourceMap: true }
                         },
                         {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true,
+                                plugins: function () {
+                                    return [autoprefixer('last 2 versions', 'ie 10')];
+                                }
+                            }
+                        },
+                        {
                             loader: 'sass-loader',
                             options: { sourceMap: true }
-                        },
+                        }
                     ],
                 }),
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'images/[name].[ext]'
+                }
             }]
         },
         plugins: [
